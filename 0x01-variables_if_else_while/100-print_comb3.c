753abf875e30721ee0bda6bb72bbb3a0fd993ded
gmukerem <gmukerem@gmail.com>
2022-12-11 10:07:10 -0800

    tas10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,28 +6,24 @@
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int 0nes = '0';
+	int tens = '0';
 
-	while (d < 10)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		c = 0;
-		while (c < 10)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			if (d != c && d < c)
+			if (!((ones == tens) || (tens > ones)))
 			{
-				putchar('0' + 'd');
-				putchar('0' + 'c');
-
-				if (c + d != 17)
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '0' && tens == '0'))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			c++;
 		}
-		d++;
 	}
 	putchar('\n');
 	return (0);
