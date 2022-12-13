5f585dc6c8ac802cad8070d9e5e76ed449835035
gmukerem <gmukerem@gmail.com>
2022-12-13 12:12:19 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la = 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
