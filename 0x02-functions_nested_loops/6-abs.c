96f7f1fe77f3442bb4f857c8c9b8415ff8f87666
gmukerem <gmukerem@gmail.com>
2022-12-13 21:33:12 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @int: the value to be computed
+ * Return: absolute value
+ */
+int _abs(int)
+
+{
+	if (int >= 0)
+		return (int);
+	else
+		return (-int);
+}
+
